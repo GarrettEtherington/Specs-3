be56c040cf006fdb87fee1a4342719414ac26120
Garrett Etherington <pogglesnash@gmail.com>
2024-04-05 12:00:19 -0600

    Jon Soup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/**/*.{jsx, js}",
+            "./public/**/*.{js, jsx, html}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
